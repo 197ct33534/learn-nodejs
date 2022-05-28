e09d68d93b6685f15f585da013e74d79e32960e0
Nghia Tran <trantrungnghia07122001@gmail.com>
2022-05-28 18:34:08 +0700

    08-TUT

---
+++ 08TUT/routes/api/employes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const data = {};
+data.employes = require('../../data/employes.json');
+router
+  .route('/')
+  .get((req, res) => {
+    res.json(data.employes);
+  })
+  .post((req, res) => {
+    res.json({firstname: req.body.firstname, lastname: req.body.lastname});
+  })
+  .put((req, res) => {
+    res.json({firstname: req.body.firstname, lastname: req.body.lastname});
+  })
+  .delete((req, res) => {
+    res.json({id: req.body.id});
+  });
+
+router.route('/:id').get((req, res) => {
+  res.json({id: req.params.id});
+});
+module.exports = router;
